8ebe3f1c3ab74efe18a4b73b57735e01df7a2371
olaleykhan <realjoy9@gmail.com>
2024-06-05 13:52:12 +0100

    vue is sweet! how did I ever forget?

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <li class="nav-item"> hoho</li>
+  </nav>
+
+  <RouterView />
+</template>
+
+<style scoped>
+  .main {
+    color:red
+  }
+  .nav-item {
+    color: blue;
+  }
+  
+</style>
