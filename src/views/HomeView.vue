e2a1f51642ab6a803aba92511988533320a84777
olaleykhan <realjoy9@gmail.com>
2024-06-05 13:52:27 +0100

    ...

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import {ref} from 'vue'
+
+const fName = ref("Olalekan O.G!!")
+const formIsValid = ref()
+
+function handleSubmit(e:Event) {
+  e.preventDefault()
+  console.log(fName.value)
+}
+
+</script>
+
+<template>
+  <main class="main">
+   hello world! {{ fName }}
+
+   <form :onsubmit="handleSubmit" >
+      <input type="text" v-model="fName" placeholder="Enter your name" />
+      <button :disabled="formIsValid" >Submit</button>
+   </form>
+   
+  </main>
+</template>
+
+<style scoped>
+
+.main {
+  color: red;
+}
+
+</style>
